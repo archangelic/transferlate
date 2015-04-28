64aa4de50e01d36048462eb45e2853e315fc73c3
Michael Hancock <michaelhancock89@gmail.com>
2015-04-28 17:23:29 -0400

    Added config for which blog to post to.

--- tumblr.py
+++ tumblr.py
@@ -5,6 +5,7 @@ consumer_key = tconf['consumer_key']
 consumer_secret = tconf['consumer_secret']
 oauth_token = tconf['oauth_token']
 oauth_secret = tconf['oauth_secret']
+blog_host = tconf['blog_host']
 
 client = pytumblr.TumblrRestClient(
 	consumer_key,
@@ -19,4 +20,4 @@ with open('translate.out') as output:
 quote = quote.replace('--!--','\n')
 trans_tags = trans_tags.split(',')
 	
-client.create_photo('transferlate', state="published", tags=trans_tags, source=url, link=flickr, caption=quote)
+client.create_photo(blog_host, state="published", tags=trans_tags, source=url, link=flickr, caption=quote)
