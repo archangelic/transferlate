00ce6a7366d40c2f378c688c70da1c08d986bc22
Michael Hancock <michaelhancock89@gmail.com>
2016-06-20 14:32:06 -0700

    First database migration test

--- setup_db.py
+++ setup_db.py
@@ -9,37 +9,34 @@ def build_database():
 
     cur.execute(('CREATE TABLE IF NOT EXISTS Photos'
                  '(Id INTEGER PRIMARY KEY, photo_id INTEGER, user TEXT, url TEXT,'
-                 ' width INTEGER, height INTEGER'))
-    cur.execute(('CREATE TABLE IF NOT EXISTS Licenses'
-                 '(Id INTEGER PRIMARY KEY, lic_id INTEGER, name TEXT, url TEXT)'))
-    cur.execute(('CREATE TABLE IF NOT EXISTS Languages'
-                 '(Id INTEGER PRIMARY KEY, code TEXT, language TEXT)'))
+                 ' width INTEGER, height INTEGER, tags TEXT'))
     cur.execute(('CREATE TABLE IF NOT EXISTS Words'
                  '(Id INTEGER PRIMARY KEY, word TEXT)'))
     cur.execute(("CREATE TABLE IF NOT EXISTS old_photos"
                  "(Id INTEGER PRIMARY KEY, photo INTEGER)"))
 
-    with open(os.path.join('database', 'languages.txt'), 'r') as langs:
-        langlist = langs.read().splitlines()
-    for each in langlist:
-        language, code = each.split(',')
-        cur.execute('INSERT INTO Languages(code,language) VALUES(?,?)',
-                    (code, language))
-    cur.execute("SELECT * FROM Languages")
-    data = cur.fetchall()
-    for each in data:
-        print(each)
-
     with open(os.path.join('database', 'wordlist.txt'), 'r') as words:
         wordlist = words.read().splitlines()
     for each in wordlist:
         cur.execute('INSERT INTO Words(word) VALUES(?)', (each,))
-    cur.execute("SELECT * FROM Words")
-    data = cur.fetchall()
-    for each in data:
-        print(each)
 
     con.commit()
+
+
+def migrate():
+    os.rename("soup.db", "backup.db")
+    build_database()
+    bk = lite.connect('backup.db')
+    sp = lite.connect('soup.db')
+    bcur = bk.cursor()
+    scur = sp.cursor()
+    bcur.execute("SELECT photo_id, user, url, width, height, tags FROM Photos")
+    photos = bcur.fetchall()
+    for each in photos:
+        # scur.execute('INSERT INTO Photos(photo_id, user, url, width, height, tags) VALUES(?, ?, ?, ?, ?, ?)',
+                    #  tuple(each))
+        print(each)
+
 
 if __name__ == "__main__":
     build_database()
